9a270d8bdda321f52b9755cd03bd32b984ebebd7
grada92 <danielegradassai@gmail.com>
2022-09-12 14:49:45 +0200

    BONUS

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log('JS OK')
+
+const root = new Vue({
+
+    el: '#root',
+    data: {
+        titletext: 'Nuovo Titolo', // aggiungo title da Vue
+        image: 'img/01.jpg', // BONUS 
+
+    }
+}
+)
